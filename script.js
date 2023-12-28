a6b4b0c1ffb9531a69bbbf46655c5b39d9a0ddd9
Eren <akiferenyilmaz@gmail.com>
2023-12-28 12:01:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Set the date we're counting up from
-    var countUpDate = new Date("Jan 1, 2023 00:00:00").getTime();
+document.addEventListener('DOMContentLoaded', function () {
+    // Set the date when Katherine & Eren crossed paths
+    const startDate = new Date('2023-01-01T00:00:00Z');
 
-    // Update the count-up every 1 second
-    var x = setInterval(function () {
-        // Get the current date and time
-        var now = new Date().getTime();
+    function updateTimer() {
+        const currentDate = new Date();
+        const timeDifference = currentDate - startDate;
 
-        // Calculate the elapsed time
-        var elapsedTime = now - countUpDate;
+        const seconds = Math.floor((timeDifference / 1000) % 60);
+        const minutes = Math.floor((timeDifference / (1000 * 60)) % 60);
+        const hours = Math.floor((timeDifference / (1000 * 60 * 60)) % 24);
 
-        // Calculate days, hours, minutes, and seconds
-        var days = Math.floor(elapsedTime / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((elapsedTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+        document.getElementById('timer').innerText = `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`;
+    }
 
-        // Display the count-up
-        document.getElementById("countup").innerHTML =
-            days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-    }, 1000);
+    function formatTime(time) {
+        return time < 10 ? `0${time}` : time;
+    }
+
+    // Update the timer every second
+    setInterval(updateTimer, 1000);
+
+    // Initial update
+    updateTimer();
 });
