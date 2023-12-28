2c32bdc31dc7bfc9f31ea354fe1a738e26685c94
Eren <akiferenyilmaz@gmail.com>
2023-12-28 12:28:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Set the imaginary date when Katherine & Eren crossed paths (Dec 16th)
     const startDate = new Date('2022-12-16T00:00:00Z');
+    let clickCount = 0;
 
     function updateTimer() {
         const currentDate = new Date();
@@ -24,4 +25,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Initial update
     updateTimer();
+
+    // Heart Click Event
+    const heart = document.getElementById('heart');
+    const heartContainer = document.getElementById('heartContainer');
+    const message = document.getElementById('message');
+
+    heartContainer.addEventListener('click', function () {
+        clickCount++;
+
+        if (clickCount < 21) {
+            heart.classList.add('clicked');
+
+            // Remove the 'clicked' class after the animation
+            setTimeout(function () {
+                heart.classList.remove('clicked');
+            }, 300);
+        } else {
+            // Show message after 21 clicks
+            message.classList.remove('hidden');
+        }
+    });
 });
